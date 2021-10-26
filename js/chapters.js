a710cdb2c973777e77b1003b43617f7abc118319
Pascal Seckinger <pascal.seckinger@protonmail.com>
2021-10-26 09:07:30 +0200

    chapters style and javascript + first chapter

---
+++ js/chapters.js
@@ -0,0 +1,12 @@
+$(function(){
+  $('#header').load('../../common/navbar.html')
+});
+
+MathJax = {
+  tex: {
+    inlineMath: [['$', '$'], ['\\(', '\\)']]
+  },
+  svg: {
+    fontCache: 'global'
+  }
+};
